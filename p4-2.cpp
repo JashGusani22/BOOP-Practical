570ba277b1c13dd123c8fbd9d791ef87d07d7725
JashGusani22 <162081452+JashGusani22@users.noreply.github.com>
2024-03-21 07:50:01 +0530

    Create p4-2.cpp

---
+++ p4-2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+    int Multiplication(int x,int y){
+        return x*y;
+    }
+int main(){
+    int a;
+    int b;
+    int Ans;
+    
+    cout<<"Enter Two Number:- ";
+    cin>>a>>b;
+    
+    Ans = Multiplication(a,b);
+    
+    cout<<"Give Number of Multiplication is = "<<Ans;
+    
+}
