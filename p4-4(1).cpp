f1f74c47ede7927cf287a63687c835639057368c
JashGusani22 <162081452+JashGusani22@users.noreply.github.com>
2024-03-21 07:52:01 +0530

    Create p4-4(1).cpp

---
+++ p4-4(1).cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+int max(int x , int y , int z){
+        if(x > y && x > z){
+            cout << "a "<<x<<" is Greater" << endl;
+        }else if(y > x && y > z){
+            cout << "b " <<y<<" is Greater" << endl;
+        }else if(z > x && z > y){
+            cout << "C " <<z<<" is Greater" << endl;
+        }
+}
+string max(string x , string y , string z){
+    int la=x.length();
+    int lb=y.length(); 
+    int lc=z.length();
+        if(la > lb && la > lc){
+  return x;
+        }else if(lb > la && lb > lc){
+      return y;
+        }else if(lc > la && lc > lb){
+            return z;
+        }
+}
+
+int main()
+{
+    int a,b,c;
+    cout << "Enter Three number - " ;
+    cin>>a>>b>>c;
+      
+    cout << max(a,b,c)<< endl;
+    return 0;
+}
